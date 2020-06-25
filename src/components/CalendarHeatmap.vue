bda973c05c41652a7409a0362477526a30a684f0
David Hood <dave.hood@corvid-paygate.com>
2020-06-25 10:48:33 +0100

    Removed Past Condition on heatmap

--- src/components/CalendarHeatmap.vue
+++ src/components/CalendarHeatmap.vue
@@ -46,8 +46,7 @@
           :transform="getWeekPosition(weekIndex)"
         )
           rect.vch__day__square(
-            v-for="(day, dayIndex) in week",
-            v-if="day.date < now"
+            v-for="(day, dayIndex) in week",            
             :key="dayIndex",
             :transform="getDayPosition(dayIndex)"
             :width="SQUARE_SIZE - SQUARE_BORDER_SIZE",
